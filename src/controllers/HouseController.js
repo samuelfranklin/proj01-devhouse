8a668805bb92f0cb1244a1ebfc087b7f59b202f8
samuelfranklin <samuel.franklins@gmail.com>
2020-07-25 12:59:56 -0300

    create method update on house controller

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,18 @@
 import House from '../models/House';
+import User from '../models/User';
 
 class HouseController {
+
+    async index(req, res) {
+        const { status } = req.query;
+        const houses = await House.find({ status });
+
+        return res.json(houses);
+
+        
+        return res.json({ ok: false });
+    }
+
     async store(req, res) {
         const { filename } = req.file;
         const { description, price, location, status } = req.body;
@@ -16,6 +28,31 @@ class HouseController {
         });
 
         return res.json(house);
+    }
+
+    async update(req, res) {
+        const { filename } = req.file;
+        const { house_id } = req.params;
+        const { description, price, location, status } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+        const house = await House.findById(house_id);
+
+        if(String(user_id) !== String(house.user)) {
+            return res.status(401).json({ error: "ERROR: Você não tem permissão para alterar este registro!" });
+        }
+
+        await House.updateOne({ _id: house_id }, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price, 
+            location,
+            status,
+        });
+
+        return res.json({ message: "Informações sobre a casa alteradas com sucesso!" });
     }
 }
 
